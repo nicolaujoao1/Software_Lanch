68422231bf38b2d6109a95815dee0d9d51a157d0
nicolaujoao1 <ilolanicolau1999@gmail.com>
2022-10-25 14:03:34 +0100

    Add project files.

---
+++ Software_Lanch/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Software_Lanch.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
