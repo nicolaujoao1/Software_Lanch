5e8012e2f38a0d3fe66481957cb3382e5c6fba4b
nicolaujoao1 <ilolanicolau1999@gmail.com>
2022-11-01 09:42:52 +0100

    Criar os DbSets e Construtor

---
+++ Software_Lanch/Context/AppDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Software_Lanch.Models;
+
+namespace Software_Lanch.Context
+{
+    public class AppDbContext:DbContext
+    {
+        #region DbSet<T>
+        public DbSet<Categoria> Categorias { get; set; }
+        public DbSet<Lanch> Lanchs { get; set; }
+        #endregion
+        #region Ctor
+            public AppDbContext(DbContextOptions<AppDbContext>option):base(option){ }
+        #endregion
+        
+    }
+}
